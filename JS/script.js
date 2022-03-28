fa2f5d0762aef6ff3bafe1c5ad3a58dcfb174d74
Tom Boyer <boyer.tom21@gmail.com>
2022-03-28 14:24:11 +0200

    footer + overflow content

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,35 @@
 // Page 1 airplane
 
-let airplane = document.querySelector('.airplane')
-let go = document.querySelector('.go')
-let cache = document.querySelector('.cache')
-let reload = document.querySelector('.reload')
+let airplane = document.querySelector(".airplane");
+let go = document.querySelector(".go");
+let cache = document.querySelector(".cache");
+let reload = document.querySelector(".reload");
 
-go.addEventListener('click', ()=>{
-    airplane.classList.add('active')
-    cache.classList.add('active')
-    reload.classList.remove('active')
-})
+go.addEventListener("click", () => {
+  airplane.classList.add("active");
+  cache.classList.add("active");
+  reload.classList.remove("active");
+});
 
-reload.addEventListener('click', ()=>{
-    airplane.classList.remove('active')
-    cache.classList.remove('active')
-    reload.classList.add('active')
-})
+reload.addEventListener("click", () => {
+  airplane.classList.remove("active");
+  cache.classList.remove("active");
+  reload.classList.add("active");
+});
+
+let bicycle = document.querySelector(".bicycle");
+let go2 = document.querySelector(".go2");
+let cache2 = document.querySelector(".cache2");
+let reload2 = document.querySelector(".reload2");
+
+go2.addEventListener("click", () => {
+  bicycle.classList.add("active");
+  cache2.classList.add("active");
+  reload2.classList.remove("active");
+});
+
+reload2.addEventListener("click", () => {
+  bicycle.classList.remove("active");
+  cache2.classList.remove("active");
+  reload2.classList.add("active");
+});
